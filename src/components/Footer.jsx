ae99f3fca26d63ff680573e9068d3c8f97b386ed
programmerNizam <mdnizam12109@gmail.com>
2024-01-19 22:18:44 +0600

    [Done] Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,12 @@
 export default function Footer() {
-  return <div>Footer</div>;
+  const year = new Date().getFullYear();
+  return (
+    <footer className="py-6 md:py-8">
+      <div className="container mx-auto">
+        <p className="text-center text-sm lg:text-base">
+          Copyright ©2023-{year} | All rights reserved by ProgrammerNizam
+        </p>
+      </div>
+    </footer>
+  );
 }
