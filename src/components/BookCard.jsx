1cf42d5d35f01043eef9ca9ef437ec6e51c2d69d
programmerNizam <mdnizam12109@gmail.com>
2024-01-19 22:50:05 +0600

    [Update] Load book data on book card component

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -19,11 +19,7 @@ export default function BookCard({ book }) {
           <h4 className="text-lg font-bold lg:text-xl">${price}</h4>
 
           <div className="flex items-center space-x-1">
-            <img src="./assets/star.svg" />
-            <img src="./assets/star.svg" />
-            <img src="./assets/star.svg" />
-            <img src="./assets/star.svg" />
-            <span className="text-xs lg:text-sm">(4 Star)</span>
+            <span className="text-xs lg:text-sm">({rating} Star)</span>
           </div>
         </div>
 
@@ -45,7 +41,7 @@ export default function BookCard({ book }) {
             </svg>
             Add to Cart
           </button>
-          <button className="flex min-w-[132px] items-center justify-center gap-1 rounded-md bg-[#1C4336]/[14%] py-1.5 text-[#1C4336] transition-all hover:bg-[#1C4336]/[24%] lg:py-1.5">
+          <button className="flex min-w-[132px] items-center justify-center gap-1 rounded-md bg-[#1C4336]/[14%] py-1.5 text-[#1C4336] transition-all hover:bg-[#1C4336]/[24%] lg:py-1.5" >
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
@@ -60,7 +56,7 @@ export default function BookCard({ book }) {
                 d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
               />
             </svg>
-            Favourite
+            Favorite
           </button>
         </div>
       </div>
