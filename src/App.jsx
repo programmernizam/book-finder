d7610510ee46caab92fc8889525da8706956ec55
programmerNizam <mdnizam12109@gmail.com>
2024-01-19 22:05:43 +0600

    Create Header Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
+import Header from "./components/Header";
+
 export default function App() {
   return (
-    <div className="text-red-500">App</div>
-  )
+    <>
+      <Header />
+    </>
+  );
 }
