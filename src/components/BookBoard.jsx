6ab49ba08923d4d8fc397816595a2b535dde8a94
programmerNizam <mdnizam12109@gmail.com>
2024-01-19 22:22:37 +0600

    [Update] Search Box Design and Structure content added

--- src/components/BookBoard.jsx
+++ src/components/BookBoard.jsx
@@ -1,67 +1,9 @@
+import SearchBox from "./SearchBox";
+
 export default function BookBoard() {
   return (
     <main className="my-10 lg:my-14">
-      <header className="mb-8 lg:mb-10 mx-auto max-w-7xl">
-        <div className="mx-auto flex items-end justify-between max-md:max-w-[95%] max-md:flex-col max-md:items-start max-md:space-y-4">
-          <div>
-            <h6 className="mb-2 text-base lg:text-xl">Trending on 2021</h6>
-            <h2 className="mb-6 font-['Playfair_Display'] text-3xl font-bold lg:text-4xl">
-              Trending Books of the Year
-            </h2>
-
-            <form>
-              <div className="flex">
-                <div className="relative w-full overflow-hidden rounded-lg border-2 border-[#1C4336] text-[#1C4336] md:min-w-[380px] lg:min-w-[440px]">
-                  <input
-                    type="search"
-                    id="search-dropdown"
-                    className="z-20 block w-full bg-white px-4 py-2.5 pr-10 text-[#1C4336] placeholder:text-[#1C4336] focus:outline-none"
-                    placeholder="Search Book"
-                    required
-                  />
-                  <div className="absolute right-0 top-0 flex h-full items-center">
-                    <button
-                      type="submit"
-                      className="mr-1.5 flex items-center space-x-1.5 rounded-md rounded-e-lg bg-[#1C4336] px-4 py-2.5 text-sm text-white"
-                    >
-                      <svg
-                        className="h-[14px] w-[14px]"
-                        aria-hidden="true"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 20 20"
-                      >
-                        <path
-                          stroke="currentColor"
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth="2"
-                          d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-                        />
-                      </svg>
-                      <span>Search</span>
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </form>
-          </div>
-
-          <div className="flex items-stretch space-x-3">
-            <select
-              className="cursor-pointer rounded-md border px-4 py-2 text-center text-gray-600"
-              name="sortBy"
-              id="sortBy"
-            >
-              <option value="">Sort</option>
-              <option value="name_asc">Name (A-Z)</option>
-              <option value="name_desc">Name (Z-A)</option>
-              <option value="year_asc">Publication Year (Oldest)</option>
-              <option value="year_desc">Publication Year (Newest)</option>
-            </select>
-          </div>
-        </div>
-      </header>
+      <SearchBox />
       <div className="container mx-auto grid grid-cols-1 gap-8 max-w-7xl md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
         <div className="space-y-3">
           <div className="flex items-center justify-center rounded-md border border-[#324251]/30 bg-white p-4">
@@ -73,7 +15,9 @@ export default function BookBoard() {
           </div>
 
           <div className="space-y-3">
-            <h4 className="text-lg font-bold lg:text-xl">JavaScript and Jquery</h4>
+            <h4 className="text-lg font-bold lg:text-xl">
+              JavaScript and Jquery
+            </h4>
             <p className="text-xs lg:text-sm">
               By : <span>Jon Duckett</span>
             </p>
@@ -138,7 +82,9 @@ export default function BookBoard() {
           </div>
 
           <div className="space-y-3">
-            <h4 className="text-lg font-bold lg:text-xl">JavaScript and Jquery</h4>
+            <h4 className="text-lg font-bold lg:text-xl">
+              JavaScript and Jquery
+            </h4>
             <p className="text-xs lg:text-sm">
               By : <span>Jon Duckett</span>
             </p>
@@ -209,7 +155,9 @@ export default function BookBoard() {
           </div>
 
           <div className="space-y-3">
-            <h4 className="text-lg font-bold lg:text-xl">JavaScript and Jquery</h4>
+            <h4 className="text-lg font-bold lg:text-xl">
+              JavaScript and Jquery
+            </h4>
             <p className="text-xs lg:text-sm">
               By : <span>Jon Duckett</span>
             </p>
@@ -274,7 +222,9 @@ export default function BookBoard() {
           </div>
 
           <div className="space-y-3">
-            <h4 className="text-lg font-bold lg:text-xl">JavaScript and Jquery</h4>
+            <h4 className="text-lg font-bold lg:text-xl">
+              JavaScript and Jquery
+            </h4>
             <p className="text-xs lg:text-sm">
               By : <span>Jon Duckett</span>
             </p>
@@ -344,7 +294,9 @@ export default function BookBoard() {
           </div>
 
           <div className="space-y-3">
-            <h4 className="text-lg font-bold lg:text-xl">JavaScript and Jquery</h4>
+            <h4 className="text-lg font-bold lg:text-xl">
+              JavaScript and Jquery
+            </h4>
             <p className="text-xs lg:text-sm">
               By : <span>Jon Duckett</span>
             </p>
@@ -409,7 +361,9 @@ export default function BookBoard() {
           </div>
 
           <div className="space-y-3">
-            <h4 className="text-lg font-bold lg:text-xl">JavaScript and Jquery</h4>
+            <h4 className="text-lg font-bold lg:text-xl">
+              JavaScript and Jquery
+            </h4>
             <p className="text-xs lg:text-sm">
               By : <span>Jon Duckett</span>
             </p>
@@ -479,7 +433,9 @@ export default function BookBoard() {
           </div>
 
           <div className="space-y-3">
-            <h4 className="text-lg font-bold lg:text-xl">JavaScript and Jquery</h4>
+            <h4 className="text-lg font-bold lg:text-xl">
+              JavaScript and Jquery
+            </h4>
             <p className="text-xs lg:text-sm">
               By : <span>Jon Duckett</span>
             </p>
@@ -544,7 +500,9 @@ export default function BookBoard() {
           </div>
 
           <div className="space-y-3">
-            <h4 className="text-lg font-bold lg:text-xl">JavaScript and Jquery</h4>
+            <h4 className="text-lg font-bold lg:text-xl">
+              JavaScript and Jquery
+            </h4>
             <p className="text-xs lg:text-sm">
               By : <span>Jon Duckett</span>
             </p>
