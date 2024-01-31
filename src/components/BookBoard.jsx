f5f09bf0caf595c9d98431a2b51be44119971b15
Md Nizam Uddin <88489743+programmernizam@users.noreply.github.com>
2024-01-31 12:53:36 +0600

    Update BookBoard.jsx

--- src/components/BookBoard.jsx
+++ src/components/BookBoard.jsx
@@ -51,7 +51,7 @@ export default function BookBoard() {
       <div className="container mx-auto grid grid-cols-1 gap-8 max-w-7xl md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
         {books.map((book) => (
           <BookCard key={book.id} book={book} />
-        ))}
+        ))} 
       </div>
     </main>
   );
